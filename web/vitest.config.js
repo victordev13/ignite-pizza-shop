fc7d606c40f6627c340465920e72b1533f9a4b14
victordev13 <vtrcarvalho.13@gmail.com>
2024-02-19 19:37:27 -0300

    fix(test): vitest should find alias, include `test` dir

--- web/vitest.config.js
+++ web/vitest.config.js
@@ -1,7 +1,14 @@
 /// <reference types="vitest" />
+import path from 'node:path'
+
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
     setupFiles: ['./test/setup.ts'],
