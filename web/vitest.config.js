845d928f8058df20f50da9ee29f1b4239a5456f5
victordev13 <vtrcarvalho.13@gmail.com>
2024-02-19 19:28:37 -0300

    feat: setup vitest
    ```
    pnpm i vitest @testing-library/jest-dom @testing-library/react happy-dom -D
    ```

---
+++ web/vitest.config.js
@@ -0,0 +1,10 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+})
