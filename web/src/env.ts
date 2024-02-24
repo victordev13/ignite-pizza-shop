520a283a168416888eee7d2283792a17f7358217
victordev13 <vtrcarvalho.13@gmail.com>
2024-02-24 20:52:36 -0300

    feat: setup MSW (Api mock)

--- web/src/env.ts
+++ web/src/env.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 const truthyValues = ['1', 'true']
 
 const envSchema = z.object({
+  MODE: z.enum(['production', 'development', 'test']),
   VITE_API_URL: z.string().url(),
   VITE_API_USE_RESPONSE_DELAY: z
     .string()
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,11 +3,21 @@ import './global.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { enableMSW } from './api/mock/index.ts'
 import { App } from './app.tsx'
+import { env } from './env'
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const render = () => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
+
+if (env.MODE !== 'test') {
+  render()
+} else {
+  enableMSW().then(() => render())
+}
