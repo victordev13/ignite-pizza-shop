4bf07a55f65abda533fae3ba014aa9e7ac02e76f
victordev13 <vtrcarvalho.13@gmail.com>
2024-04-24 22:18:55 -0300

    test(e2e): sign-in and sign-up

---
+++ web/test/sign-up.e2e.spec.ts
@@ -0,0 +1,39 @@
+import { expect, test } from '@playwright/test'
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Nome do estabelecimento').fill('Pizza Shop')
+  await page.getByLabel('Seu nome').fill('John Doe')
+  await page.getByLabel('Seu e-mail').fill('johndoe@example.com')
+  await page.getByLabel('Seu celular').fill('123456678901')
+
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  const toast = page.getByText('Restaurante cadastrado com sucesso!')
+
+  expect(toast).toBeVisible()
+})
+
+test('sign up with error', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Nome do estabelecimento').fill('Invalid name')
+  await page.getByLabel('Seu nome').fill('John Doe')
+  await page.getByLabel('Seu e-mail').fill('johndoe@example.com')
+  await page.getByLabel('Seu celular').fill('123812641264')
+
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  const toast = page.getByText('Ocorreu um erro interno 😩!')
+
+  expect(toast).toBeVisible()
+})
+
+test('navigate to login page', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Já tenho conta' }).click()
+
+  expect(page.url()).toContain('/sign-in')
+})
